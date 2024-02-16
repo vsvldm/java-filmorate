e31e7419fc511308d6d245d96d6e4e3ea0489ae9
Vsevolod <vsevolodmakarsky@yandex.ru>
2024-02-16 15:49:49 +0700

    feat: реализация контроллеров приложения

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,47 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exceptions.InvalidDataInputException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@Slf4j
 public class FilmController {
+    private final List<Film> films = new ArrayList<>();
+    private int id = 0;
+    @GetMapping("/films")
+    public List<Film> getFilms() {
+        return films;
+    }
+
+    @PostMapping(value = "/films")
+    public Film createFilm(@Valid @RequestBody Film film) {
+        film.setId(++id);
+        films.add(film);
+        log.info("Фильм {} успешно создан", film.getName());
+        return film;
+    }
+
+    @PutMapping("/films")
+    public ResponseEntity<Film> updateFilm (@Valid @RequestBody Film film) {
+        try {
+            if (films.stream().anyMatch(filmFromList -> film.getId() == filmFromList.getId())) {
+                films.removeIf(userFromList -> film.getId() == userFromList.getId());
+                films.add(film);
+                log.info("Фильм с id = {} успешно обновлен", film.getId());
+            } else {
+                throw new InvalidDataInputException("Нет фильма с таким id = " + film.getId());
+            }
+        } catch (InvalidDataInputException e) {
+            log.warn(e.getMessage());
+            return  ResponseEntity.internalServerError().body(film);
+        }
+        return ResponseEntity.ok(film);
+    }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,51 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exceptions.InvalidDataInputException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@Slf4j
+public class UserController {
+    private final List<User> users = new ArrayList<>();
+    private int id = 0;
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return users;
+    }
+
+    @PostMapping(value = "/users")
+    public User createUser(@Valid @RequestBody User user) {
+        if (user.getName() == null){
+            log.info("Отсутствует имя пользоваителя. Будет использован login = {} в качестве имени пользователя.", user.getLogin());
+            user.setName(user.getLogin());
+        }
+        user.setId(++id);
+        users.add(user);
+        log.info("Пользователь с login = {} успешно создан.", user.getLogin());
+        return user;
+    }
+
+    @PutMapping("/users")
+    public ResponseEntity<User> updateUser (@Valid @RequestBody User user) {
+        try {
+            if (users.stream().anyMatch(userFromList -> user.getId() == userFromList.getId())) {
+                users.removeIf(userFromList -> user.getId() == userFromList.getId());
+                users.add(user);
+                log.info("Пользовател с id = {} успешно обновлен.", user.getId());
+            } else {
+                throw new InvalidDataInputException("Нет пользователя с таким id = " + user.getId());
+            }
+        } catch (InvalidDataInputException e) {
+            log.warn(e.getMessage());
+            return ResponseEntity.internalServerError().body(user);
+        }
+        return ResponseEntity.ok(user);
+    }
+}
