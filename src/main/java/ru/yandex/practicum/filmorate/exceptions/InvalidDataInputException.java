ce89421ceda13694960412ab5524fd81bfde9b3a
Vsevolod <vsevolodmakarsky@yandex.ru>
2024-02-16 15:50:03 +0700

    feat: реализация исключения

---
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/InvalidDataInputException.java
@@ -0,0 +1,10 @@
+package ru.yandex.practicum.filmorate.exceptions;
+
+public class InvalidDataInputException extends Exception{
+    public InvalidDataInputException() {
+    }
+
+    public InvalidDataInputException(String message) {
+        super(message);
+    }
+}
