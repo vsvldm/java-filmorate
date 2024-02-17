9905983a4dd6bd91298edc74ed81a3eb3cd6582d
Vsevolod <vsevolodmakarsky@yandex.ru>
2024-02-17 23:58:34 +0700

    refactor: замена родительского класса

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/InvalidDataInputException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/InvalidDataInputException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
-public class InvalidDataInputException extends Exception {
+public class InvalidDataInputException extends RuntimeException {
     public InvalidDataInputException() {
 
     }
