b9d8253d3585bdb4f4f73bebbcb3e61cc3e3ffd2
Vsevolod <vsevolodmakarsky@yandex.ru>
2024-04-10 01:13:36 +0700

    feat: реализация интерфейса

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/review/ReviewService.java
@@ -0,0 +1,25 @@
+package ru.yandex.practicum.filmorate.service.review;
+
+import ru.yandex.practicum.filmorate.model.Review;
+
+import java.util.List;
+
+public interface ReviewService {
+    public Review create(Review review);
+
+    public Review update(Review review);
+
+    public void removeById(int reviewId);
+
+    public Review findById(int reviewId);
+
+    public List<Review> findAllByFilm(int filmId, int count);
+
+    public Review addLike(int reviewId, int userId);
+
+    public Review addDislike(int reviewId, int userId);
+
+    public void removeLike(int reviewId, int userId);
+
+    public void removeDislike(int reviewId, int userId);
+}
