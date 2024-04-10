3ad6860ade6ca1c0440d5e2c4c33e1ff98081b14
Mishail Nikolaev <mishail.nikolaev2017@yandex.ru>
2024-04-10 19:11:27 +0300

    fix

--- src/main/java/ru/yandex/practicum/filmorate/repository/genre/GenreRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/genre/GenreRepository.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.repository.genre;
 import ru.yandex.practicum.filmorate.model.Genre;
 
 import java.util.Collection;
-import java.util.HashSet;
 
 public interface GenreRepository {
     public Collection<Genre> values();
