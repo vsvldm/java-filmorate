ec93b10abdb8471ab778b8e2dcf48284a20d4b18
Mishail Nikolaev <mishail.nikolaev2017@yandex.ru>
2024-04-14 19:34:50 +0300

    fix

--- src/main/java/ru/yandex/practicum/filmorate/repository/film/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/film/InMemoryFilmStorage.java
@@ -80,8 +80,7 @@ public class InMemoryFilmStorage implements FilmStorage {
     }
 
     @Override
-    public List<Film> getRecommendations(int userId)
-    {
+    public List<Film> getRecommendations(int userId) {
         return null;
     }
 }
