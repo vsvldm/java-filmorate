5bf216a2eda0be97091cf7bc1507cfa94d5af3b4
MaxxMurygin <maxx.murygin@gmail.com>
2024-04-15 01:39:13 +0700

    fix: Remove DirectorRepository dependency from FilmDbStorage

--- src/main/java/ru/yandex/practicum/filmorate/repository/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/film/FilmDbStorage.java
@@ -12,7 +12,6 @@ import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Mpa;
-import ru.yandex.practicum.filmorate.repository.director.DirectorRepository;
 
 import java.sql.Date;
 import java.sql.PreparedStatement;
